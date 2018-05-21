e0234488ef27461cb0e0ebf2711e3e29b2d599b3
Paul Murphy <volksreserve@gmail.com>
2018-05-21 16:01:03 +0000

    Update if-statement.js

--- if-statement.js
+++ if-statement.js
@@ -8,3 +8,15 @@ if (a==b) {
 else {
    alert("No, they're NOT equal");
 }
+
+if (a == b) //equality
+if (a != b) //not equal to
+if (a === b) //strict equality
+if (a !== b) //strictly not equal
+if (a > b) //greater than
+if (a < b) //less than
+if (a >= b) //greater or equal to
+if (a <= b) //less or equal to
+   
+if (a === b && c === d) //both conditions strictly equal to be TRUE
+if (a === b || c === d) // either conditions strictly equal to be TRUE
