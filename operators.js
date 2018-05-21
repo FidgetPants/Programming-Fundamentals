3167d922bbc03ddef2da2ebc0919be8c06212c3c
Paul Murphy <volksreserve@gmail.com>
2018-05-21 15:18:04 +0000

    Update operators.js

--- operators.js
+++ operators.js
@@ -5,7 +5,7 @@ var result2 = b - a;
 var result3 = a * b;
 var result4 = b / a;
 
-alert result1;
-alert result2;
-alert result3;
-alert result4;
+alert (result1);
+alert (result2);
+alert (result3);
+alert (result4);
